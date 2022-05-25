a670c967dd3fded417ac727b17b63bd445558b45
yaasien-s <Yaasiensalaam@gmail.com>
2022-05-25 21:29:18 +0200

    complete

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  
+  <Toggle />
+</template>
+
+<script>
+import Toggle from './components/Toggle.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Toggle
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ src/components/Toggle.vue
@@ -0,0 +1,48 @@
+<template>
+  <div :class="theme === 'light' ? 'light-theme' : 'dark-theme'" class="main-body">
+    <div style="padding: 5px;">
+      <div id="heading">Theme Selection</div>
+      <button @click="changeTheme">Switch</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toggle',
+  data() {
+    return{
+      theme: "light"
+    }
+  },
+  methods: {
+    changeTheme() {
+      console.log("Theme Changed");
+      this.theme = this.theme === "dark" ? "light" : "dark";
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-body{
+  height: 100vh;
+  padding: 0;
+}
+.light-theme{
+  background: white;
+  color: black;
+}
+.dark-theme{
+  background: black;
+  color: white;
+}
+button{
+  padding: 10px;
+}
+#heading{
+  font-size: 3rem;
+  font-weight: bolder;
+}
+</style>
